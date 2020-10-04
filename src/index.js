8756edcee8ccaa144f5adacfd919cb805066fae7
yusuf hammadi <ucup1745@gmail.com>
2020-10-05 00:52:21 +0700

    Penambahan Redux di registration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {rootReducer} from './redux/reducers/rootReducer';
+//store
+const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
---
+++ src/redux/reducers/registerReducer.js
@@ -0,0 +1,16 @@
+import { userConstants } from '../types';
+
+export function register(state = {}, action) {
+  switch (action.type) {
+    case userConstants.REGISTER_REQUEST:
+      return { registering: true };
+    case userConstants.REGISTER_SUCCESS:
+      return {
+        newUser: action.newUser
+      };
+    case userConstants.REGISTER_FAILURE:
+      return {};
+    default:
+      return state
+  }
+}
