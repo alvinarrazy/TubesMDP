cc921046b33d98c7c2f461b9ea104f1add4023db
Alvin Arrazy <51030862+alvinarrazy@users.noreply.github.com>
2020-12-02 22:12:13 +0700

    pwa added

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    API: 'http://localhost:3001/api',
-    STATIC_PATH: 'http://localhost:3001/'
+    // API: 'http://localhost:3001/api',
+    // API: 'http://192.168.43.221:3001/api',
+    //API: 'http://192.168.100.10:3001/api',
+    API: 'http://32eb69a7e82c.ngrok.io/api',
+    //STATIC_PATH: 'http://localhost:3001/',
+    STATIC_PATH: 'http://32eb69a7e82c.ngrok.io/',
 };
